f7f078ae7837f06031d4700dcad222af4446835c
rbadri91 <rbadri01@gmail.com>
2024-07-28 21:02:47 -0500

    Addibng db name

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "url": 'mongodb+srv://rbadri01:sIfAmUEW8BNnL1XD@themoviesstopauthentica.vzxw7hj.mongodb.net/?retryWrites=true&w=majority&appName=TheMoviesStopAuthentication'
+    "url": 'mongodb+srv://rbadri01:sIfAmUEW8BNnL1XD@themoviesstopauthentica.vzxw7hj.mongodb.net/themoviesStopAuth?retryWrites=true&w=majority&appName=TheMoviesStopAuthentication'
 };
